09dbbe589744ecab5b4f6873f7ae0d30d1b6d3ef
qu1ief <sabirkg@code.edu.az>
2025-06-15 10:47:48 +0400

    Order Signal R

--- PustokTask/Areas/Manage/Controllers/OrderController.cs
+++ PustokTask/Areas/Manage/Controllers/OrderController.cs
@@ -1,14 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
 using PustokTask.Data;
+using PustokTask.Hubs;
 
 namespace PustokTask.Areas.Manage.Controllers
 {
     [Area("Manage")]
-    public class OrderController(PustokDbContex dbContex) : Controller
+    public class OrderController(PustokDbContex dbContex,
+       IHubContext<ChatHub> hubContext ) : Controller
     {
         public IActionResult Index()
         {
-            return View();
+            return View(dbContex.Orders.Include(a=>a.AppUser).ToList());
+        }
+
+        public IActionResult Accept(int id)
+        {
+            var order = dbContex.Orders.Include(o=>o.AppUser).FirstOrDefault(o => o.Id == id);
+            order.Status = Models.OrderStatus.Accepted;
+
+            dbContex.SaveChanges();
+            hubContext.Clients.Client(order.AppUser.ConnectionId).SendAsync("OrderAccepted");
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Reject(int id)
+        {
+            var order = dbContex.Orders.FirstOrDefault(o => o.Id == id);
+            order.Status = Models.OrderStatus.Rejected;
+
+            dbContex.SaveChanges();
+            hubContext.Clients.Client(order.AppUser.ConnectionId).SendAsync("OrderRejected");
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
